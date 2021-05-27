6fcf3fba2eae94bfddd1bb52080106b6b8cefae7
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-27 20:00:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   new_array = []
   
-  holiday_hash.each do |season, holiday_hash
+  holiday_hash.each do |season, holiday_hash|
+    
 end
 
 
