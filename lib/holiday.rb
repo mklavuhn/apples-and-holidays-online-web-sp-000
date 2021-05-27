a5445e915329e08ea818debdb9bde694967596f9
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-27 19:50:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday_hash|
     puts "#{season.to_s.capitalize}:"
-    
+    holiday_hash.each do |
 end
 
 def all_holidays_with_bbq(holiday_hash)
