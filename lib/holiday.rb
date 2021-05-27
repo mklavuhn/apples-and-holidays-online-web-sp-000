e898325f4f293429039131938dda5e4b6ee84a75
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-27 20:01:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday_hash|
     holiday_hash.each do |holiday_name, supply_array|
       holiday_hash.each do |supply|
+        if supply == "BBQ"
 end
 
 
